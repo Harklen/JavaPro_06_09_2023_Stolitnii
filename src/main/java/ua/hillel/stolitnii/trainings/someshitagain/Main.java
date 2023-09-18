62f8e838f13b032315d92a6c1092bb689a7bab0b
Harklen <as.harklen@gmail.com>
2023-09-18 18:02:33 +0300

    abstract default implements

---
+++ src/main/java/ua/hillel/stolitnii/trainings/someshitagain/Main.java
@@ -0,0 +1,99 @@
+package ua.hillel.stolitnii.trainings.someshitagain;
+
+interface Сheese {
+    default void chedder() {
+        System.out.println("Chedder");
+    }
+    default void parmesan() {
+        System.out.println("Parmesan");
+    }
+    default void mozzarella() {
+        System.out.println("Mozzarella");
+    }
+
+    default void dorblue(){
+        System.out.println("Dorblue");
+    }
+}
+abstract class Pizza {
+    abstract void filling();
+    abstract void sauce();
+    abstract void cheese();
+    abstract void dough();
+    abstract void bake();
+}
+class BlankPizza extends Pizza {
+
+    @Override
+    void dough(){
+        System.out.println("Тонке");
+    }
+    @Override
+    void sauce(){
+        System.out.println("Томатна");
+    }
+    @Override
+    void cheese(){
+        System.out.println("Моцарелла");
+    }
+    @Override
+    void filling(){
+        System.out.println("Шинка");
+    }
+    @Override
+    void bake(){
+        System.out.println("В духовці");
+    }
+}
+class FourCheeses extends BlankPizza implements Сheese {
+    @Override
+    void cheese(){
+    }
+    @Override
+    void sauce(){
+        System.out.println("Сметанний соус");
+    }
+}
+class Margarita extends BlankPizza {
+    @Override
+    void filling(){
+        System.out.println("Салямі");
+    }
+    @Override
+    void cheese() {
+    }
+}
+class Pasta implements Сheese {
+    void pasta(){
+        System.out.println("Паста");
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Сьогодні в меню:");
+        System.out.println("Піца 4 сири:");
+        FourCheeses fourCheeses = new FourCheeses();
+        fourCheeses.dough();
+        fourCheeses.sauce();
+        fourCheeses.mozzarella();
+        fourCheeses.chedder();
+        fourCheeses.parmesan();
+        fourCheeses.dorblue();
+        fourCheeses.filling();
+        fourCheeses.bake();
+        System.out.println("Піца Маргарита:");
+        Margarita margarita = new Margarita();
+        margarita.dough();
+        margarita.sauce();
+        margarita.cheese();
+        margarita.filling();
+        margarita.bake();
+        System.out.println("Паста:");
+        Pasta pasta = new Pasta();
+        pasta.pasta();
+        pasta.mozzarella();
+        pasta.parmesan();
+    }
+}
