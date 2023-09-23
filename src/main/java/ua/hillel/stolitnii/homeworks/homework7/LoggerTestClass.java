9861b876aacad23c19f4293be21826f81afdaaea
harklen <as.harklen@gmail.com>
2023-09-24 02:25:58 +0300

    homework 7 completed

---
+++ src/main/java/ua/hillel/stolitnii/homeworks/homework7/LoggerTestClass.java
@@ -0,0 +1,55 @@
+package ua.hillel.stolitnii.homeworks.homework7;
+import java.io.*;
+import java.util.logging.*;
+
+public class LoggerTestClass {
+
+    public static void createLogFile(String fileName) {
+        try {
+            File logFile = new File(fileName);
+            if (logFile.createNewFile()) {
+                System.out.println("Файл " + fileName + " створено.");
+            } else {
+                System.out.println("Файл " + fileName + " вже існує.");
+                System.out.println("Видаляємо старий лог " + fileName);
+                     logFile.delete();
+                    }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeLogMessage(String level, String message) {
+        try (FileWriter writer = new FileWriter("log.txt", true);
+             BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
+            bufferedWriter.write(level + ": " + message);
+            bufferedWriter.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readLogMessages() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("log.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void filterAndPrintLogMessages(String logLevel) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("log.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith(logLevel)) {
+                    System.out.println(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/ua/hillel/stolitnii/homeworks/homework7/Main.java
@@ -0,0 +1,95 @@
+package ua.hillel.stolitnii.homeworks.homework7;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import static ua.hillel.stolitnii.homeworks.homework7.LoggerTestClass.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Результат: " + Calculator.calculator());
+        readLogMessages();
+        filterAndPrintLogMessages("INFO, DEBUG, ERROR");
+    }
+}
+
+class Calculator {
+    public static int calculator() {
+
+        createLogFile("log.txt");
+        writeLogMessage("INFO", "Calculator started");
+        int result = 0, a = 0, b = 0;
+        String operation;
+
+        Scanner scanner = new Scanner(System.in);
+
+        writeLogMessage("INFO", "Getting first number");
+        do {
+            try {
+                System.out.print("Введіть перше число: ");
+                a = scanner.nextInt();
+                System.out.println("Ви ввели число: " + a);
+                writeLogMessage("DEBUG", "User entered first " + a);
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Ви ввели не ціле число. Будь ласка, введіть ціле число.");
+                writeLogMessage("ERROR", "User entered not a number");
+                scanner.nextLine();
+            }
+        } while (true);
+
+        scanner.nextLine();
+        do {
+            System.out.print("Введіть операцію (+, -, *, /): ");
+            operation = scanner.nextLine();
+            if (!operation.equals("+") && !operation.equals("-") && !operation.equals("*") && !operation.equals("/")) {
+                System.out.println("Ви обрали не вірну операцію. Будь ласка, введіть операцію (+, -, *, /).");
+                writeLogMessage("ERROR", "User entered wrong operation");
+            } else {
+                System.out.println("Ви обрали: " + operation);
+                writeLogMessage("DEBUG", "User choosed " + operation);
+                break;
+            }
+        } while (true);
+
+        do {
+            try {
+                System.out.print("Введіть друге число: ");
+                b = scanner.nextInt();
+                System.out.println("Ви ввели число: " + b);
+                writeLogMessage("DEBUG", "User entered second " + b);
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Ви ввели не ціле число. Будь ласка, введіть ціле число.");
+                writeLogMessage("ERROR", "User entered not a number");
+                scanner.nextLine();
+            }
+        } while (true);
+
+        switch (operation) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                if (b != 0) {
+                    result = a / b;
+                } else {
+                    System.out.println("Помилка: Ділення на нуль.");
+                    writeLogMessage("ERROR", "Division by zero");
+                }
+                break;
+            default:
+                System.out.println("Помилка: Невідома операція.");
+                writeLogMessage("ERROR", "Unknown operation");
+        }
+
+        scanner.close();
+        return result;
+    }
+}
