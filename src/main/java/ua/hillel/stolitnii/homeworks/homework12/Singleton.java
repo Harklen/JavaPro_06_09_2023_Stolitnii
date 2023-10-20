21988a1f068a01c838d2f008267b56366caad0b8
Harklen <as.harklen@gmail.com>
2023-10-20 22:07:00 +0300

    Homework 12 completed

---
+++ src/main/java/ua/hillel/stolitnii/homeworks/homework12/Singleton.java
@@ -0,0 +1,15 @@
+package ua.hillel.stolitnii.homeworks.homework12;
+
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
---
+++ src/main/java/ua/hillel/stolitnii/homeworks/homework12/ThreadSafeSingleton.java
@@ -0,0 +1,15 @@
+package ua.hillel.stolitnii.homeworks.homework12;
+
+public class ThreadSafeSingleton {
+    private static ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton() {
+    }
+
+    public static synchronized ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            instance = new ThreadSafeSingleton();
+        }
+        return instance;
+    }
+}
